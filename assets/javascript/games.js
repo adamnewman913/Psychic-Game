69d9ca1e9ce90f0b341b4f427e310bbf3d923c05
Adam Newman <anewman1@ku.edu>
2019-06-17 16:23:47 -0500

    modified a few CSS details and leanred I suck at JavaScript currently

---
+++ assets/javascript/games.js
@@ -0,0 +1,14 @@
+var computerchoices = 
+    [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var lettersGuessed = [];
+var computerGuess = [];
+
+function guess() {
+document.getElementById ("userGuess").onkeypress.innerhtml = "numer"
+}
+
+
+
