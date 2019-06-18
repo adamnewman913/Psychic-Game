928444b4a17d1a6b9258b7a5fe2e2ab7398763dc
Adam Newman <anewman1@ku.edu>
2019-06-18 16:39:04 -0500

    made strides towards functional Javas

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,14 +1,55 @@
-var computerchoices = 
-    [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                 "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
-var lettersGuessed = [];
-var computerGuess = [];
+var left = 9;
+var guesses = 9;
+var guessesSoFar = [];
 
-function guess() {
-document.getElementById ("userGuess").onkeypress.innerhtml = "numer"
+var wins = 0;
+var losses = 0;
+var left = 9;
+var guesses = 9;
+var guessesSoFar = [];
+var psychicLetter;
+
+
+var newGuess = function() {
+    psychicLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+};
+
+var TriesSoFar = function() {
+    document.getElementById("guesses").innerHTML = "Guesses so far: " + guessesSoFar.join(",");
+};
+
+var guessesLeft = function() {
+    document.getElementById("left").innerHTML = "Guesses Left: " + left;
+};
+
+var newGame = function() {
+	guessedLetters = [];
+    left = 9;
+    newLetter();
+    guessesLeft();
+    TriesSoFar();
 }
 
-
-
+document.onkeyup = function(event) {
+	var userGuess = event.key;
+    left--;
+    guessesSoFar.push(userGuess);
+    TriesSoFar();
+    guessesLeft();
+    if (left > 0) {
+        if (userGuess == psychicLetter) {
+        	wins++;
+        	document.getElementById("wins").innerHTML = "Wins:" + wins;
+            newGame();
+        }
+    } else if (left == 0) {
+    	losses++;
+    	document.getElementById("losses").innerHTML = "Losses:" + losses;
+        newGame();
+    }
+};
